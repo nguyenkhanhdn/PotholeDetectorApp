8517900dfccf30ca3846f88ce1cd8e99157dee70
Khanh Nguyen <khanhdsp@gmail.com>
2023-03-21 21:45:33 +0700

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PotholeDetectorApp.Startup))]
+namespace PotholeDetectorApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
